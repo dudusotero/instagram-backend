6911e0cd06d24bd22c44c9ec500ee499540c4141
Eduardo Sotero <eduardo@feracode.com>
2019-06-14 11:32:36 -0300

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const { MONGODB, PORT } = process.env;
 
 mongoose.connect(MONGODB, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 app.use((req, res, next) => {
@@ -21,11 +21,8 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.use(
-  "/files",
-  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
-);
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
-app.use(require("./routes"));
+app.use(require('./routes'));
 
 server.listen(PORT || 3333);
